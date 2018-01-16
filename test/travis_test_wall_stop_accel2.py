fd44cb01351d310de0a30d53d45e5ffbc192bf00
Kohei Kyoya <k.kyoya1217@gmail.com>
2018-01-16 19:47:11 +0900

    finally version

--- test/travis_test_wall_stop_accel2.py
+++ test/travis_test_wall_stop_accel2.py
@@ -5,19 +5,21 @@ import time, sys
 from std_msgs.msg import UInt16
 from std_srvs.srv import Trigger, TriggerResponse
 
-class WallStopTest(unittest.TestCase):
-    def set_and_get(self, lf, ls, rs, rf):
-        with open("/dev/rtlightsensor0", "w") as f:
+class WallStopAccelTest(unittest.TestCase):
+    def test_node_exist(self):
+        nodes = rosnode.get_node_names()
+        self.assertIn('/wall_stop_accel',nodes, "node does not exist")
+
+    def set_sensor_values(self,lf,ls,rs,rf):
+        with open("/dev/rtlightsensor0","w") as f:
             f.write("%d %d %d %d\n" % (rf,rs,ls,lf))
 
-        time.sleep(0.3)
-
-        with open("/dev/rtmotor_raw_l0", "r") as lf,\
-             open("/dev/rtmotor_raw_r0", "r") as rf:
+    def get_freqs(self):
+        with open("/dev/rtmotor_raw_l0","r") as lf,\
+             open("/dev/rtmotor_raw_r0","r") as rf:
             left = int(lf.readline().rstrip())
-            right= int(rf.readline().rstrip())
-
-        return left, right
+            right = int(rf.readline().rstrip())
+        return left,right
 
     def test_io(self):
         left, right = self.set_and_get(400,100,100,0) #total:600
@@ -31,8 +33,9 @@ class WallStopTest(unittest.TestCase):
         self.assertTrue(2000 < left == right, "can't accerelate")
 
         left, right = self.set_and_get(15,0,20,15) #total:50
+        self.assertTrue(left == right == 0, "can't stop again")
 
 if __name__ == '__main__':
     time.sleep(3)
-    rospy.init_node('travis_test_wall_stop')
-    rostest.rosrun('pimouse_run_corridor','travis_test_wall_stop',WallStopTest)
+    rospy.init_node('travis_test_wall_stop_accel')
+    rostest.rosrun('pimouse_run_corridor','travis_test_wall_stop_accel',WallStopAccelTest)
