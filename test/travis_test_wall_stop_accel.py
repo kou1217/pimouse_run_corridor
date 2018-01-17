96d559b92db8c13af3c2a99cd4f2b7918ecd6b7b
Kohei Kyoya <k.kyoya1217@gmail.com>
2018-01-17 11:09:17 +0900

    modified sleep time

--- test/travis_test_wall_stop_accel.py
+++ test/travis_test_wall_stop_accel.py
@@ -26,11 +26,12 @@ class WallStopTest(unittest.TestCase):
         left, right = self.set_and_get(40,0,0,9) #total:49
         self.assertTrue(0 < left == right < 1000, "can't move again")
 
-        time.sleep(0.3)
+        time.sleep(5.0)
         left, right = self.set_and_get(40,0,0,9) #total:49
         self.assertTrue(2000 < left == right, "can't accerelate")
 
         left, right = self.set_and_get(15,0,20,15) #total:50
+        self.assertTrue(left == right == 0, "can't stop again")
 
 if __name__ == '__main__':
     time.sleep(3)
